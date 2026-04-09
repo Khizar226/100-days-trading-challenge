fd409378a170243cdb17e54a45a71ae69f4582db
Khizar226 <39296017+Khizar226@users.noreply.github.com>
2026-04-09 21:19:20 +0500

    Add Firebase configuration for trading challenge

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration for 100 Day Trading Challenge
+// This file contains the Firebase project configuration
+
+const firebaseConfig = {
+    apiKey:            "AIzaSyDzz2x5Has4erBj_ssq0eUcngIwZBsDX9g",
+    authDomain:        "trading-challenge-f86f9.firebaseapp.com",
+    projectId:         "trading-challenge-f86f9",
+    storageBucket:     "trading-challenge-f86f9.firebasestorage.app",
+    messagingSenderId: "365594936857",
+    appId:             "1:365594936857:web:d09a8365d208d5fad24c88"
+};
+
+export default firebaseConfig;
